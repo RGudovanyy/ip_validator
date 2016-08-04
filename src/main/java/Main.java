020be2e1393dc66b5f9ddcef970498e8acf77625
rgudovanyy <rgudovanyy@yandex.ru>
2016-08-04 17:43:30 +0300

    Added tests and method to validate range borders

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,87 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
-        List<String> tempArr = new ArrayList<>();
-        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String firstAddress = "192.168.0.0";
-        String secondAddress = "192.169.0.255";
+    public static void main(String[] args){
+        List<String> resultList;
+        String firstAddress = "";
+        String secondAddress = "";
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            firstAddress = reader.readLine();
+            secondAddress = reader.readLine();
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка ввода/вывода " + e.getMessage());
+        }
 
         int [] address1 = parseAddress(firstAddress);
         int [] address2 = parseAddress(secondAddress);
-        Long t0 = System.currentTimeMillis();
+        //Long t0 = System.currentTimeMillis();
 
-        tempArr = fillDiapasonArray(address1,address2);
+        validateAdresses(address1,address2);
 
-        System.out.println(tempArr.size());
+        resultList = fillDiapasonArray(address1,address2);
 
-        for(String s : tempArr)
+        for(String s : resultList)
             System.out.println(s);
 
-        Long t1 = System.currentTimeMillis();
-        System.out.println("Time: " + (t1 - t0)/1000d + " sec.");
+        //Long t1 = System.currentTimeMillis();
+        //System.out.println("Time: " + (t1 - t0)/1000d + " sec.");
     }
 
-    private static int[] parseAddress(String address){
+    public static int[] parseAddress(String address){
         int [] addressArray = new int[4];
         for(int index = 0; index < 4; index++){
             int nextOctet = Integer.parseInt(address.split("\\.")[index]);
             if(nextOctet > 255) {
                 System.out.println("ip адрес "+ address +" неверен. Октет " + index + " должен быть меньше или равен 255");
-                System.exit(1);
+                return null;
             }
             addressArray[index] = nextOctet;
         }
         return addressArray;
     }
 
-    private static List<String> fillDiapasonArray(int[] firstAddr, int[] secondAddr){
+    private static void validateAdresses(int[] firstAddress, int[] secondAddress){
+        if(firstAddress[0] > secondAddress[0]) {
+            System.out.println("Первый октет второго адреса больше первого октета первого адреса");
+            System.exit(1);
+        }
+        else {
+            if(firstAddress[1] > secondAddress[1]) {
+                System.out.println("Второй октет второго адреса больше второго октета первого адреса");
+                System.exit(1);
+            }
+            else {
+                if(firstAddress[2] > secondAddress[2]) {
+                    System.out.println("Третий октет второго адреса больше третьего октета первого адреса");
+                    System.exit(1);
+                }
+                else {
+                    if(firstAddress[3] > secondAddress[3]){
+                        System.out.println("Четвертый октет второго адреса больше четвертого октета первого адреса");
+                        System.exit(1);
+                    }
+                }
+            }
+        }
+    }
+
+    public static List<String> fillDiapasonArray(int[] firstAddress, int[] secondAddress){
         List<String> diapason = new ArrayList<>();
-        int fourth = firstAddr[3];
-        int third = firstAddr[2];
-        int second = firstAddr[1];
-        int first = firstAddr[0];
+
+
+        int fourth = firstAddress[3];
+        int third = firstAddress[2];
+        int second = firstAddress[1];
+        int first = firstAddress[0];
         while (true){
-            if(first == secondAddr[0] & second == secondAddr[1] & third == secondAddr[2] & fourth == secondAddr[3])
+            if(first == secondAddress[0] & second == secondAddress[1] & third == secondAddress[2] & fourth == secondAddress[3])
                 break;
             else {
                 fourth++;
